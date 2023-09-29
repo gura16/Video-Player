93e25d12dc8241095b05451b1c9548842ba31ce1
guramikvakhadze <gkvaxadze@gmail.com>
2023-09-29 21:51:07 +0100

    add video

--- Video-Player/src/App.tsx
+++ Video-Player/src/App.tsx
@@ -1,30 +1,28 @@
 import { useRef, useState } from "react";
+import styled from "styled-components";
+const videoUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f3/Big_Buck_Bunny_first_23_seconds_1080p.ogv/Big_Buck_Bunny_first_23_seconds_1080p.ogv.720p.vp9.webm";
 
 function App() {
-  const [count, setCount] = useState<any>(0);
-
-  const buttonRef = useRef<any>(0);
-
   return (
     <div>
-      <button
-        onClick={() => {
-          console.log("დაეჭირა პირველ ღილაკს");
-        }}
-        ref={buttonRef}
-      >
-        button 1
-      </button>
-      <button
-        onClick={() => {
-          console.log("დაეჭირა მეორე ღილაკს");
-          buttonRef.current.click();
-        }}
-      >
-        button 2
-      </button>
+      <Maindiv>
+        <Video controls>
+          <source src={videoUrl} type="video/webm" />
+        </Video>
+      </Maindiv>
     </div>
   );
 }
 
 export default App;
+
+const Video = styled.video`
+  width: 500px;
+  height: 500px;
+`;
+
+const Maindiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
