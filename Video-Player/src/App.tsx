5131cc1b35af25160b49ed34b009091fd96fa0ba
guramikvakhadze <gkvaxadze@gmail.com>
2023-09-30 17:29:13 +0100

    add play/pause

--- Video-Player/src/App.tsx
+++ Video-Player/src/App.tsx
@@ -1,16 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 const videoUrl =
   "https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f3/Big_Buck_Bunny_first_23_seconds_1080p.ogv/Big_Buck_Bunny_first_23_seconds_1080p.ogv.720p.vp9.webm";
 
 function App() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current?.addEventListener("timeupdate", (event) => {
+      console.log(event.target.currentTime);
+    });
+  }, []);
+
   return (
     <div>
       <Maindiv>
-        <Video controls>
+        <Video ref={videoRef}>
           <source src={videoUrl} type="video/webm" />
         </Video>
       </Maindiv>
+      <button
+        onClick={() => {
+          if (videoRef.current?.paused) {
+            videoRef.current?.play();
+          } else {
+            videoRef.current?.pause();
+          }
+        }}
+      >
+        Play/Pause
+      </button>
     </div>
   );
 }
@@ -19,7 +38,7 @@ export default App;
 
 const Video = styled.video`
   width: 500px;
-  height: 500px;
+  height: 300px;
 `;
 
 const Maindiv = styled.div`
